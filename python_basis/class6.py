9d5013bb169b3ea6d22a1efd6f5d4cf79eafd743
sxqyyds <15815185+sxqyyds@user.noreply.gitee.com>
2025-07-14 10:54:46 +0800

    9

--- python_basis/class6.py
+++ python_basis/class6.py
@@ -5,8 +5,16 @@ for i in s:
     print(i)
 
 num = 10
-for j in range(1,100,2):
+for j in range(0,100,1):
+    if(j%2==0):
+        continue
+    elif(j%3==0):
+        continue
+    elif(j%5==0):
+        continue
+    elif(j%7==0):
+        continue
     print(j)
-
+#包前不包后原则
 
 
